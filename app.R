3a08a0cf341970092ffeab2d6c8e80019f53d232
Matthijs Berends <m.s.berends@umcg.nl>
2026-03-02 11:16:02 +0100

    fixes

--- app.R
+++ app.R
@@ -1,15 +1,15 @@
-library(shiny)
 library(plot2)
+library(shiny)
 
 tryCatch({
-  plot2:::create_interactively(hide_generated_code = TRUE, hide_export_buttons = FALSE, pretty_labels = TRUE)
+  plot2::create_interactively(hide_generated_code = TRUE, hide_export_buttons = FALSE, pretty_labels = TRUE, upload_tab = TRUE)
 }, error = function(e) {
   # Provide a helpful error message in the UI
   ui <- fluidPage(
-    titlePanel("plot2.shiny - Configuration Error"),
+    titlePanel("Configuration Error"),
     mainPanel(
       h3("Unable to load the application"),
-      p("There was an error loading the Shiny app from the 'plot2.shiny' package."),
+      p("There was an error loading the Shiny app."),
       h4("Error message:"),
       pre(as.character(e)),
       hr(),
@@ -17,7 +17,7 @@ tryCatch({
       tags$ul(
         tags$li("The ", code("plot2"), " package is properly installed"),
         tags$li("The package runs ", code("create_interactively()"), " without error"),
-        tags$li("All dependencies are available")
+        tags$li("All dependencies are available; run ", code("plot2:::install_shiny_deps()"), " to confirm"),
       )
     )
   )
